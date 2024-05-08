4de9e08fd2676cf8f63899e01e0b5ed38ede92da
TheVerl <brendantarnold05@gmail.com>
2024-05-08 19:41:04 +1000

    giving the ec2 an extra gig of ram made it stop complaining, time to bring back all the code

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,42 +1,42 @@
 import { Elysia, t } from 'elysia'
-// import { html } from '@elysiajs/html'
+import { html } from '@elysiajs/html'
 
 
 new Elysia()
-    // .use(html())
-    .get('/jsx', () => ('hi'
-        // <html lang='en'>
-        //     <head>
-        //         <title>verlBlog</title>
-        //         <script src="https://unpkg.com/htmx.org@1.9.12"></script>
-        //         <link href="http://localhost:3000/assets/style.css" rel="stylesheet" />
-        //     </head>
-        //     <body>
-        //         <div class="headerTitle">
-        //             <h1>verlBlog</h1>
-        //         </div>
-        //         <div id="statusList" />
+    .use(html())
+    .get('/jsx', () => (
+        <html lang='en'>
+            <head>
+                <title>verlBlog</title>
+                <script src="https://unpkg.com/htmx.org@1.9.12"></script>
+                <link href="http://localhost:3000/assets/style.css" rel="stylesheet" />
+            </head>
+            <body>
+                <div class="headerTitle">
+                    <h1>verlBlog</h1>
+                </div>
+                <div id="statusList" />
 
-        //         <hr />
-        //         <div class="postsContainer"></div>
-        //         <form hx-post="/submit" hx-target="#statusList" hx-swap="beforeend" id="postForm" >
-        //           <div class="postBox">
-        //             <textarea class="postBoxText" rows="5" cols="30" name="postBoxText">Write something here!</textarea>
-        //             <br />
-        //             <input type="submit" class="postBoxButton" value="Submit"></input>
-        //           </div>
-        //         </form>
-        //     </body>
-        // </html>
+                <hr />
+                <div class="postsContainer"></div>
+                <form hx-post="/submit" hx-target="#statusList" hx-swap="beforeend" id="postForm" >
+                  <div class="postBox">
+                    <textarea class="postBoxText" rows="5" cols="30" name="postBoxText">Write something here!</textarea>
+                    <br />
+                    <input type="submit" class="postBoxButton" value="Submit"></input>
+                  </div>
+                </form>
+            </body>
+        </html>
     ))
-    // .post("/submit", ({body}) => {
-    //     return <div class="status"><p>{body.postBoxText}</p></div>
-    // },
-    // {
-    //     body: t.Object({
-    //         postBoxText: t.String()
-    //     })
+    .post("/submit", ({body}) => {
+        return <div class="status"><p>{body.postBoxText}</p></div>
+    },
+    {
+        body: t.Object({
+            postBoxText: t.String()
+        })
 
-    // })
-    // .get("/assets/:file", ({params: {file}}) => Bun.file(`assets/${file}`))
+    })
+    .get("/assets/:file", ({params: {file}}) => Bun.file(`assets/${file}`))
     .listen(3000)
