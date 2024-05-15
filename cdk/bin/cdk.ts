c5bce25742d07e22e8bb70555c522e6cd618f3ce
TheVerl <brendantarnold05@gmail.com>
2024-05-15 12:38:30 +1000

    gave bootstrap args for account

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -3,14 +3,8 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { CdkStack } from '../lib/cdk-stack';
 
-const resourceEnvironment: cdk.ResourceEnvironment = {
-  account: "851725333784",
-  region: "ap-southeast-2"
-}
-
 const app = new cdk.App();
 new CdkStack(app, 'stackVerlBlogTest2', {
-  env: resourceEnvironment
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
